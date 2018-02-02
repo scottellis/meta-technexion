d8fb3f65c105d5e654066ad88013982e3b988604
Scott Ellis <scott@jumpnowtek.com>
2018-02-02 10:26:08 -0500

    copy_boot script does not need /media/card

--- recipes-bsp/u-boot-scr/files/emmc-boot.cmd
+++ recipes-bsp/u-boot-scr/files/emmc-boot.cmd
@@ -1,8 +1,9 @@
 setenv bootpart 1:1
 setenv bootdir /boot
-setenv mmcroot /dev/mmcblk0p1 ro
+setenv mmcroot /dev/mmcblk2p1 ro
 setenv mmcrootfstype ext4 rootwait
 setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype}
+run findfdt
 load mmc ${bootpart} ${fdt_addr} ${bootdir}/${fdtfile}
 load mmc ${bootpart} ${loadaddr} ${bootdir}/zImage
 bootz ${loadaddr} - ${fdt_addr}
