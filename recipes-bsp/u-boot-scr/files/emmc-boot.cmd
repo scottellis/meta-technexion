986cb3573ca51b1201d9c3349c7930597c269c4d
Scott Ellis <scott@jumpnowtek.com>
2018-02-15 18:24:43 -0500

    u-boot-scr: Add display args for hdmi 1080p

--- recipes-bsp/u-boot-scr/files/emmc-boot.cmd
+++ recipes-bsp/u-boot-scr/files/emmc-boot.cmd
@@ -2,7 +2,8 @@ setenv bootpart 0:1
 setenv bootdir /boot
 setenv mmcroot /dev/mmcblk2p1 ro
 setenv mmcrootfstype ext4 rootwait
-setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype}
+setenv display video=mxcfb0:dev=hdmi,1920x1080M@60,if=RGB24 fbmem=28M
+setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype} ${display}
 run findfdt
 load mmc ${bootpart} ${fdt_addr} ${bootdir}/${fdtfile}
 load mmc ${bootpart} ${loadaddr} ${bootdir}/zImage
